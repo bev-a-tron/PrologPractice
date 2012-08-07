d5e67fd68f5aaa4e1d8d79a3c030c332d5c7598a
bev-a-tron <beverly.a.lau@gmail.com>
2012-08-07 02:22:55 -0400

    clean up remove predicate

--- test2.pl
+++ test2.pl
@@ -1,13 +1,13 @@
 %remove( ToBeRemoved, List, AfterRemoval ).
 %remove( mungo, [mungo, thinks, mungo, is, great], [thinks, is, great] ).
 
+% Y needs to either be the empty set when X = ToBeRemoved or
+% Y is ToBeRemoved if X \= Y.
+
 remove( _, [], [] ).
-remove( ToBeRemoved, [ X | Rest ], AfterRemoval ) :-
-	% Y needs to either be the empty set when X = ToBeRemoved or
-	% Y is ToBeRemoved if X \= Y.
+remove( ToBeRemoved, [ X | Rest ], [ X | AfterRemoval ] ) :-
 	X \= ToBeRemoved,
-	append( [X], AfterRemoval1, AfterRemoval ),
-	remove( ToBeRemoved, Rest, AfterRemoval1 ).
+	remove( ToBeRemoved, Rest, AfterRemoval ).
 
 remove( ToBeRemoved, [ ToBeRemoved | Rest ], AfterRemoval ) :-
 	remove( ToBeRemoved, Rest, AfterRemoval ).
