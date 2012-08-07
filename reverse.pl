bea71fd06b9ee9f5f6568de065669aa0297a88c6
bev-a-tron <beverly.a.lau@gmail.com>
2012-08-07 15:44:21 -0400

    put base case back the way it was

--- reverse.pl
+++ reverse.pl
@@ -1,7 +1,7 @@
 %reverse( Forward, Backward ).
 %reverse( [mungo, is, great], [great, is, mungo]).
 
-reverse( [A], [A] ).
+reverse( [], [] ).
 reverse( [Head | Tail], NewList ) :-
 	reverse( Tail, List ),
 	append( List, [Head], NewList ).
