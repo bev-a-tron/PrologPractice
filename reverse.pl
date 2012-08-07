9a2961fcc15b519af48718c116cc29f38f434c80
bev-a-tron <beverly.a.lau@gmail.com>
2012-08-07 15:23:49 -0400

    fix base case

--- reverse.pl
+++ reverse.pl
@@ -1,7 +1,7 @@
 %reverse( Forward, Backward ).
 %reverse( [mungo, is, great], [great, is, mungo]).
 
-reverse( [], [] ).
+reverse( [A], [A] ).
 reverse( [Head | Tail], NewList ) :-
 	append( List, [Head], NewList ),
 	reverse( Tail, List ).
