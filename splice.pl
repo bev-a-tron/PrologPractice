fd30191df0424a38dd8ec765b8ebaf3491f55490
bev-a-tron <beverly.a.lau@gmail.com>
2012-08-07 13:19:27 -0400

    done with splice

--- splice.pl
+++ splice.pl
@@ -5,3 +5,13 @@
 
 % ? - splice( [ceri, is, a, total, utter, genius], [this, is, fibs], Output ).
 % Output = [ ceri, this, is, is, a, fibs, total, utter, genius ].
+
+splice( [], [], [] ).
+splice( [ List1Head | List1 ], [ List2Head | List2 ], [ List1Head, List2Head | Answer ] ) :-
+	splice( List1, List2, Answer ).
+splice( [ List1Head | List1 ], List2, [ List1Head | Answer ] ) :-
+	List2 = [],
+	splice( List1, List2, Answer ).
+splice( List1, [ List2Head | List2 ], [ List2Head | Answer ] ) :-
+	List1 = [],
+	splice( List1, List2, Answer ).
