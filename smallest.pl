81a320d9b63faa3fcb0d78d22839c2f1a989cb93
bev-a-tron <beverly.a.lau@gmail.com>
2012-08-07 18:41:58 -0400

    smallest works

--- smallest.pl
+++ smallest.pl
@@ -1,2 +1,11 @@
 %smallest( List, Smallest ).
 %smallest( [ 20, 42, 3, 16, 2, 9 ], 2 ).
+
+smallest( [A], A ).
+smallest( [ Head | Rest ], Head ) :-
+	smallest( Rest , SmallestOfTheRest ),
+	Head < SmallestOfTheRest.
+smallest( [ Head | Rest ], Smallest ) :-
+	smallest( Rest , Smallest ),
+	Head >= Smallest.
+	
