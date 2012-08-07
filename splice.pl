d36d4df971476cf6c85529d820c0d3eb091aa39a
bev-a-tron <beverly.a.lau@gmail.com>
2012-08-07 12:54:18 -0400

    start splice exercise

---
+++ splice.pl
@@ -0,0 +1,7 @@
+%% ---------------------                                                                    
+% splice( List1, List2, SplicedList ).
+% ? - splice( [mungo, jerry, to, pub], [and, went, a], Output ).
+% Output = [ mungo, and, jerry, went, to, a pub ].
+
+% ? - splice( [ceri, is, a, total, utter, genius], [this, is, fibs], Output ).
+% Output = [ ceri, this, is, is, a, fibs, total, utter, genius ].
