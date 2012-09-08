7ae5ba31b3664398a5dc96a250f367c224b22012
bev-a-tron <beverly.a.lau@gmail.com>
2012-09-08 03:16:12 -0500

    flatten not working yet, going to sleep

--- flatten.pl
+++ flatten.pl
@@ -2,4 +2,9 @@
 % e.g. flatten( [a,[b,[c,d],e,[f,g]]], X ).
 % X = [a,b,c,d,e,f,g].
 
-%flatten( [a], [a]).
+flatten( [X], [X] ).
+%flatten( [a, [b, c] ], [a, b, c] ).
+%flatten( [[a, b], c ], [a, b, c] ).
+flatten( [ Head | Rest ], Y ) :-
+	append( Head, X, Y ),
+	flatten( [ Rest ], X ).
